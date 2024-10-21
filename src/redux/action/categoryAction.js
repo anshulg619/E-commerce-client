d761ac084a494080f8767aa65d23ee4180208c64
anshulg619 <anshulgupta394@gmail.com>
2024-10-21 17:24:02 +0530

    fixed category route

--- src/redux/action/categoryAction.js
+++ src/redux/action/categoryAction.js
@@ -4,7 +4,7 @@ import * as actionTypes from '../constant/categoryConstants'
 const url = 'https://e-commerce-backend-fkq1.onrender.com';
 export const getAllCategoriesAction = (token) => async (dispatch) => {
     try {
-        const {data} = await axios.get(`${url}/category/getAllCategories`, {headers:{'Authorization':`Bearer ${token}`}})
+        const {data} = await axios.get(`${url}/flipkart/category/getAllCategories`, {headers:{'Authorization':`Bearer ${token}`}})
         dispatch({type:actionTypes.GET_ALL_CATEGORIES, payload:data})
     } catch (error) {
         console.log(error.message)
